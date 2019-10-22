75e350a70a24e88049fd8e39e1b5e8beef0bf8d8
boazhacks <boazhacks@localhost.localdomain>
2019-10-22 13:57:45 -0400

    Fix creation API bug

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -3,9 +3,9 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Post(models.Model):
-    updated_date = models.DateTimeField('date updated')
-    published_date = models.DateTimeField('date published')
+    updated_date = models.DateTimeField('date updated', auto_now=True)
+    published_date = models.DateTimeField('date published', auto_now=True)
     content = models.TextField()
     title = models.CharField(max_length=150)
-    id = models.UUIDField(unique=True, primary_key=True, default=uuid.uuid4)
+    id = models.UUIDField(unique=True, primary_key=True, default=uuid.uuid4, editable=False)
     author = models.ForeignKey(User,on_delete=models.DO_NOTHING)
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,11 +1,9 @@
 from rest_framework import viewsets
 from posts.models import Post
 from posts.serializers import PostSerializer
+from rest_framework.response import Response
+
 
 class PostViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
     queryset = Post.objects.all()
-    print (queryset)
     serializer_class = PostSerializer
